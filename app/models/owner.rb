97f24db737157c6a1bde56fedcb85e6fc2f6af12
Stanley Louis <stanlouis@msn.com>
2016-08-04 20:26:53 -0700

    update dynamic home page

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -5,16 +5,16 @@ class Owner
 	end
 
 	def birthdate
-		birthdate = Date.new(1974, 12, 27)	
+		birthdate = Date.new(1974, 12, 27)
 	end
 
 	def countdown
 		today = Date.today
 		birthday = Date.new(today.year, birthdate.month, birthdate.day)
 		if birthday > today
-			countdown = (birthday.next_year - today).to_i
+			countdown = (birthday - today).to_i
 		else
-			countdown = (birthday.next_year =today).to_i
+			countdown = (birthday.next_year - today).to_i
 		end
 	end
 end
